e2a9d16ce35289d2ec854021d5250c0ead31b9f6
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-22 15:55:25 +0100

    Add number extraction logic

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,33 @@ function App() {
 		for (let i = 0; i < 90; i++) {
 			numbers.push(false);
 		}
+
 		setTombola(numbers);
+	};
+
+	const getRandomNumber = (min, max) =>
+		Math.floor(Math.random() * (max - min)) + min;
+
+	const handleClick = () => {
+		const newTombola = [...tombola];
+		let randomNumber = getRandomNumber(1, 90);
+		while (newTombola[randomNumber - 1]) {
+			randomNumber = randomNumber(1, 90);
+		}
+		console.log(randomNumber);
+		newTombola[randomNumber - 1] = true;
+		setTombola(newTombola);
 	};
 
 	useEffect(inizializzaTombola, []);
 
 	return (
 		<>
+			<button type="button" onClick={handleClick}>
+				Estrai
+			</button>
 			{tombola.map((current, index) => (
-				<div key={`btn-${index}`}>
+				<div key={`btn-${index}-${Date.now()}`}>
 					{index + 1} {current.toString()}
 				</div>
 			))}
