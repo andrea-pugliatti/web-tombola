8662ec757d7af5088ffaf59b0c1a74611b8c307b
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-22 12:29:12 +0100

    Initialize Tombola array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
-	return <></>;
+	const [tombola, setTombola] = useState([]);
+
+	const inizializzaTombola = () => {
+		const numbers = [];
+
+		for (let i = 0; i < 90; i++) {
+			numbers.push(false);
+		}
+		setTombola(numbers);
+	};
+
+	useEffect(inizializzaTombola, []);
+
+	return (
+		<>
+			{tombola.map((current, index) => (
+				<div key={`btn-${index}`}>
+					{index + 1} {current.toString()}
+				</div>
+			))}
+		</>
+	);
 }
 
 export default App;
